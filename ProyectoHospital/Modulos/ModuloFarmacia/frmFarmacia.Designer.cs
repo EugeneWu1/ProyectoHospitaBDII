657ab18a1c1f710c52e5ad6c2040ceefeb13efcb
KedGarci4 <kedwingarcia1@gmail.com>
2024-12-05 13:58:43 -0600

    CRUD agregado al Inventario de Farmacia.

--- ProyectoHospital/Modulos/ModuloFarmacia/frmFarmacia.Designer.cs
+++ ProyectoHospital/Modulos/ModuloFarmacia/frmFarmacia.Designer.cs
@@ -34,21 +34,25 @@
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.dgvInventarioFarmacia = new System.Windows.Forms.DataGridView();
-            this.insertButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
             this.updateButton = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.pacienteID = new System.Windows.Forms.Label();
-            this.consultID = new System.Windows.Forms.TextBox();
-            this.direc = new System.Windows.Forms.TextBox();
-            this.name = new System.Windows.Forms.TextBox();
-            this.ProductoID = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtProductoID = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dtpFechaVencimiento = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExistencia = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbTipo = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnVolver)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -104,57 +108,61 @@
             // dgvInventarioFarmacia
             // 
             this.dgvInventarioFarmacia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInventarioFarmacia.Location = new System.Drawing.Point(516, 93);
+            this.dgvInventarioFarmacia.Location = new System.Drawing.Point(326, 93);
             this.dgvInventarioFarmacia.Name = "dgvInventarioFarmacia";
-            this.dgvInventarioFarmacia.Size = new System.Drawing.Size(559, 412);
+            this.dgvInventarioFarmacia.Size = new System.Drawing.Size(762, 412);
             this.dgvInventarioFarmacia.TabIndex = 1;
             // 
-            // insertButton
+            // btnConsultar
             // 
-            this.insertButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.insertButton.Location = new System.Drawing.Point(12, 461);
-            this.insertButton.Name = "insertButton";
-            this.insertButton.Size = new System.Drawing.Size(112, 44);
-            this.insertButton.TabIndex = 216;
-            this.insertButton.Text = "CONSULTAR";
-            this.insertButton.UseVisualStyleBackColor = true;
-            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            this.btnConsultar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.Color.Black;
+            this.btnConsultar.Location = new System.Drawing.Point(10, 440);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(112, 44);
+            this.btnConsultar.TabIndex = 216;
+            this.btnConsultar.Text = "CONSULTAR";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.insertButton_Click);
             // 
-            // button1
+            // btnAgregar
             // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(135, 461);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 44);
-            this.button1.TabIndex = 217;
-            this.button1.Text = "AGREGAR";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAgregar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(144, 440);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(112, 44);
+            this.btnAgregar.TabIndex = 217;
+            this.btnAgregar.Text = "AGREGAR";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // deleteButton
             // 
             this.deleteButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.deleteButton.Location = new System.Drawing.Point(388, 461);
+            this.deleteButton.Location = new System.Drawing.Point(144, 490);
             this.deleteButton.Name = "deleteButton";
             this.deleteButton.Size = new System.Drawing.Size(112, 44);
             this.deleteButton.TabIndex = 220;
             this.deleteButton.Text = "ELIMINAR";
             this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
             // 
             // updateButton
             // 
             this.updateButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.updateButton.Location = new System.Drawing.Point(261, 461);
+            this.updateButton.Location = new System.Drawing.Point(12, 490);
             this.updateButton.Name = "updateButton";
             this.updateButton.Size = new System.Drawing.Size(112, 44);
             this.updateButton.TabIndex = 221;
             this.updateButton.Text = "EDITAR";
             this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(23, 271);
+            this.label6.Location = new System.Drawing.Point(66, 367);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(178, 21);
             this.label6.TabIndex = 230;
@@ -173,21 +181,11 @@
             // label8
             // 
             this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(23, 235);
+            this.label8.Location = new System.Drawing.Point(23, 331);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(62, 24);
             this.label8.TabIndex = 228;
             this.label8.Text = "Precio";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(23, 198);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(87, 21);
-            this.label7.TabIndex = 227;
-            this.label7.Text = "Existencia";
             // 
             // pacienteID
             // 
@@ -199,37 +197,29 @@
             this.pacienteID.TabIndex = 226;
             this.pacienteID.Text = "ProductoID";
             // 
-            // consultID
+            // txtPrecio
             // 
-            this.consultID.Location = new System.Drawing.Point(143, 235);
-            this.consultID.Multiline = true;
-            this.consultID.Name = "consultID";
-            this.consultID.Size = new System.Drawing.Size(322, 27);
-            this.consultID.TabIndex = 235;
+            this.txtPrecio.Location = new System.Drawing.Point(143, 331);
+            this.txtPrecio.Multiline = true;
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(121, 27);
+            this.txtPrecio.TabIndex = 235;
             // 
-            // direc
+            // txtNombre
             // 
-            this.direc.Location = new System.Drawing.Point(143, 198);
-            this.direc.Multiline = true;
-            this.direc.Name = "direc";
-            this.direc.Size = new System.Drawing.Size(322, 27);
-            this.direc.TabIndex = 234;
+            this.txtNombre.Location = new System.Drawing.Point(143, 157);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(121, 27);
+            this.txtNombre.TabIndex = 233;
             // 
-            // name
+            // txtProductoID
             // 
-            this.name.Location = new System.Drawing.Point(143, 157);
-            this.name.Multiline = true;
-            this.name.Name = "name";
-            this.name.Size = new System.Drawing.Size(322, 27);
-            this.name.TabIndex = 233;
-            // 
-            // ProductoID
-            // 
-            this.ProductoID.Location = new System.Drawing.Point(143, 114);
-            this.ProductoID.Multiline = true;
-            this.ProductoID.Name = "ProductoID";
-            this.ProductoID.Size = new System.Drawing.Size(322, 27);
-            this.ProductoID.TabIndex = 232;
+            this.txtProductoID.Location = new System.Drawing.Point(143, 114);
+            this.txtProductoID.Multiline = true;
+            this.txtProductoID.Name = "txtProductoID";
+            this.txtProductoID.Size = new System.Drawing.Size(121, 27);
+            this.txtProductoID.TabIndex = 232;
             // 
             // label2
             // 
@@ -241,34 +231,94 @@
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // textBox1
+            // dtpFechaVencimiento
             // 
-            this.textBox1.Location = new System.Drawing.Point(207, 274);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(258, 27);
-            this.textBox1.TabIndex = 237;
+            this.dtpFechaVencimiento.Location = new System.Drawing.Point(28, 397);
+            this.dtpFechaVencimiento.Name = "dtpFechaVencimiento";
+            this.dtpFechaVencimiento.Size = new System.Drawing.Size(258, 20);
+            this.dtpFechaVencimiento.TabIndex = 238;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 24);
+            this.label3.TabIndex = 239;
+            this.label3.Text = "Tipo";
+            // 
+            // txtExistencia
+            // 
+            this.txtExistencia.Location = new System.Drawing.Point(144, 247);
+            this.txtExistencia.Multiline = true;
+            this.txtExistencia.Name = "txtExistencia";
+            this.txtExistencia.Size = new System.Drawing.Size(121, 27);
+            this.txtExistencia.TabIndex = 242;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(24, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 21);
+            this.label7.TabIndex = 241;
+            this.label7.Text = "Existencia";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(143, 293);
+            this.txtCosto.Multiline = true;
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(121, 27);
+            this.txtCosto.TabIndex = 244;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(23, 293);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 21);
+            this.label4.TabIndex = 243;
+            this.label4.Text = "Costo";
+            // 
+            // cbTipo
+            // 
+            this.cbTipo.FormattingEnabled = true;
+            this.cbTipo.Items.AddRange(new object[] {
+            "Insumo",
+            "Medicamento",
+            "Alimento"});
+            this.cbTipo.Location = new System.Drawing.Point(143, 201);
+            this.cbTipo.Name = "cbTipo";
+            this.cbTipo.Size = new System.Drawing.Size(122, 21);
+            this.cbTipo.TabIndex = 245;
             // 
             // frmFarmacia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1128, 618);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cbTipo);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExistencia);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpFechaVencimiento);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.consultID);
-            this.Controls.Add(this.direc);
-            this.Controls.Add(this.name);
-            this.Controls.Add(this.ProductoID);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtProductoID);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.pacienteID);
             this.Controls.Add(this.updateButton);
             this.Controls.Add(this.deleteButton);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.insertButton);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.dgvInventarioFarmacia);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -293,20 +343,24 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.DataGridView dgvInventarioFarmacia;
-        private System.Windows.Forms.Button insertButton;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button deleteButton;
         private System.Windows.Forms.Button updateButton;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label pacienteID;
-        private System.Windows.Forms.TextBox consultID;
-        private System.Windows.Forms.TextBox direc;
-        private System.Windows.Forms.TextBox name;
-        private System.Windows.Forms.TextBox ProductoID;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtProductoID;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DateTimePicker dtpFechaVencimiento;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtExistencia;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbTipo;
     }
 }
--- ProyectoHospital/Modulos/ModuloFarmacia/frmFarmacia.cs
+++ ProyectoHospital/Modulos/ModuloFarmacia/frmFarmacia.cs
@@ -18,6 +18,7 @@ namespace ProyectoHospital.Modulos.Farmacia
         SqlConnection conexion;
         SqlDataAdapter adpInventario;
         DataTable tabInventario;
+
         public frmFarmacia()
         {
             InitializeComponent();
@@ -43,7 +44,42 @@ namespace ProyectoHospital.Modulos.Farmacia
 
         private void insertButton_Click(object sender, EventArgs e)
         {
+
+            if (string.IsNullOrWhiteSpace(txtNombre.Text))
+            {
+                MessageBox.Show("Por favor, ingrese un nombre para buscar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                
+                SqlCommand cmd = new SqlCommand("hospital.spBuscarProductoPorNombre", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text.Trim());
+
+                
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable resultado = new DataTable();
+                adapter.Fill(resultado);
+
+                
+                if (resultado.Rows.Count > 0)
+                {
+                    dgvInventarioFarmacia.DataSource = resultado;
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró ningún producto con ese nombre.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    dgvInventarioFarmacia.DataSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ocurrió un error al buscar el producto: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+
 
         private void MostrarDatosProducto()
         {
@@ -56,6 +92,7 @@ namespace ProyectoHospital.Modulos.Farmacia
                 tabInventario = new DataTable();
                 adpInventario.Fill(tabInventario);
                 dgvInventarioFarmacia.DataSource = tabInventario;
+                dgvInventarioFarmacia.Columns["Tipo"].HeaderText = "Tipo de Producto";
             }
             catch (Exception ex)
             {
@@ -73,5 +110,127 @@ namespace ProyectoHospital.Modulos.Farmacia
         {
             MostrarDatosProducto();
         }
+
+        private void LimpiarCampos()
+        {
+            
+            txtProductoID.Clear();
+            txtNombre.Clear();
+            txtExistencia.Clear();
+            txtCosto.Clear();
+            txtPrecio.Clear();
+            cbTipo.SelectedIndex = -1;
+            dtpFechaVencimiento.Value = DateTime.Now;
+            dgvInventarioFarmacia.ClearSelection();
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand("hospital.spInventarioInsert", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductoID", txtProductoID.Text.Trim());
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text.Trim());
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.SelectedItem?.ToString());
+                cmd.Parameters.AddWithValue("@Existencia", int.Parse(txtExistencia.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Costo", string.IsNullOrWhiteSpace(txtCosto.Text)? (object)DBNull.Value: decimal.Parse(txtCosto.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Precio", decimal.Parse(txtPrecio.Text.Trim()));
+                cmd.Parameters.AddWithValue("@fechavencimiento", dtpFechaVencimiento.Value);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable resultado = new DataTable();
+                adapter.Fill(resultado);
+
+                MessageBox.Show("Producto agregado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MostrarDatosProducto();
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al agregar el producto: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            if (dgvInventarioFarmacia.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Por favor, seleccione una fila para editar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            try
+            {
+                
+                DataGridViewRow filaSeleccionada = dgvInventarioFarmacia.SelectedRows[0];
+                int productoID = Convert.ToInt32(filaSeleccionada.Cells["ProductoID"].Value);
+
+                SqlCommand cmd = new SqlCommand("hospital.spInventarioUpdate", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@ProductoID", productoID);
+                cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text.Trim());
+                cmd.Parameters.AddWithValue("@Tipo", cbTipo.SelectedItem?.ToString());
+                cmd.Parameters.AddWithValue("@Existencia", int.Parse(txtExistencia.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Costo", string.IsNullOrWhiteSpace(txtCosto.Text) ? (object)DBNull.Value : decimal.Parse(txtCosto.Text.Trim()));
+                cmd.Parameters.AddWithValue("@Precio", decimal.Parse(txtPrecio.Text.Trim()));
+                cmd.Parameters.AddWithValue("@fechavencimiento", dtpFechaVencimiento.Value);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable resultado = new DataTable();
+                adapter.Fill(resultado);
+
+                MessageBox.Show("Producto editado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MostrarDatosProducto();
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al editar el producto: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (dgvInventarioFarmacia.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Por favor, seleccione una fila para eliminar.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            if (MessageBox.Show("¿Está seguro de que desea eliminar este producto?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+            {
+                return;
+            }
+
+            try
+            {
+                
+                DataGridViewRow filaSeleccionada = dgvInventarioFarmacia.SelectedRows[0];
+                int productoID = Convert.ToInt32(filaSeleccionada.Cells["ProductoID"].Value);
+
+                SqlCommand cmd = new SqlCommand("hospital.spInventarioDelete", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductoID", productoID);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable resultado = new DataTable();
+                adapter.Fill(resultado);
+
+                MessageBox.Show("Producto eliminado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MostrarDatosProducto();
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al eliminar el producto: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
