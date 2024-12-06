eb019580ed2e075702eaf7026d60c24256fcf8b2
Omar F Diaz <omar.padilla@grupoleitz.com>
2024-12-05 19:14:52 -0600

    oops

--- ProyectoHospital/Modulos/ModuloServicios/frmHospitalizacionAgendar.Designer.cs
+++ ProyectoHospital/Modulos/ModuloServicios/frmHospitalizacionAgendar.Designer.cs
@@ -33,8 +33,6 @@
             this.label1 = new System.Windows.Forms.Label();
             this.btnVolver = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.Date = new System.Windows.Forms.DateTimePicker();
             this.GroupBox1 = new System.Windows.Forms.GroupBox();
@@ -110,24 +108,6 @@
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(915, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(127, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "crud basico con  botones";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(661, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(248, 13);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "hospital.hospitalizacion,paciente,medico,habitacion";
             // 
             // button1
             // 
@@ -375,8 +355,6 @@
             this.Controls.Add(this.dghospitalizaciones);
             this.Controls.Add(this.cbmedico);
             this.Controls.Add(this.description);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmHospitalizacionAgendar";
@@ -403,8 +381,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox btnVolver;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DateTimePicker Date;
         internal System.Windows.Forms.GroupBox GroupBox1;
